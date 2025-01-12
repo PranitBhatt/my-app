1a2a5d98c46effb7d32d6716a8a8e79a041821ba
Pranit Bhatt <pranitbhatt333@gmail.com>
2025-01-12 12:23:23 +0530

    React basics

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <>
+    
+      <Navbar aboutText="About" title="Text Utilites" />  
+      {/* Navbar is a Component and the title and aboutText are the props for that component */}
+    </>
+  );
+}
+
+export default App;
