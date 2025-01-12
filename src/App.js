701de255142da9dc5e38b3784c1be242932c7794
Pranit Bhatt <pranitbhatt333@gmail.com>
2025-01-12 13:03:50 +0530

    Used hook useState for textarea

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
 import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
     <>
-    
-      <Navbar aboutText="About" title="Text Utilites" />  
       {/* Navbar is a Component and the title and aboutText are the props for that component */}
-    </>
+      <Navbar aboutText="About" title="Text Utilites" />  
+      <div className="container my-3">
+      <TextForm heading="Enter the text to analyze"/>
+      </div>
+      </>
   );
 }
 
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
+    const handleUpclick = ()=>{
+        console.log("UpperCase was clicked");
+        setText("You have clicked on Set it to uppercase");
+    }
+    const handleOnchange = (event)=>{
+        console.log("Handle Onchange");
+        setText(event.target.value);
+    }
+    const [text,setText] = useState('Enter text here');
+    //  setText("Enter valid text here");
   return (
     <div>
-      
+        <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnchange} id="textarea" rows="10" ></textarea>
+      </div>
+      <button className="btn btn-primary" onClick={handleUpclick}>Convert to Uppercase</button>
     </div>
   )
 }
